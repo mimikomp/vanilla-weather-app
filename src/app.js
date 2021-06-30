578be272600d7d87986f1f7cc295f64ee07c041e
mimikomp <82300671+mimikomp@users.noreply.github.com>
2021-06-29 19:43:59 -0500

    Updated Time/Date

--- src/app.js
+++ src/app.js
@@ -1,21 +1,51 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let amOrPm = "AM";
+  if (hours >= 12) {
+    amOrPm = "PM";
+    hours = hours - 12;
+  }
+  if (hours === 0) {
+    hours = "12";
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}${amOrPm}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#current-temp");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let cityElement = document.querySelector("#location-city");
-  cityElement.innerHTML = response.data.name;
   let conditionElement = document.querySelector("#condition");
-  conditionElement.innerHTML = response.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind-speed");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
   let feelsLikeElement = document.querySelector("#feels-like");
-  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
   let minTempElement = document.querySelector("#min-temp");
-  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
   let maxTempElement = document.querySelector("#max-temp");
+  let dateElement = document.querySelector("#date-time");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  conditionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
   maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "af769be7365d5beaa284c2fd49ab6ea1";
