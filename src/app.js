4184a10e305d2617021cc4dc2d6586311342f6ff
mimikomp <82300671+mimikomp@users.noreply.github.com>
2021-07-08 15:24:58 -0500

    Edited Styling

--- src/app.js
+++ src/app.js
@@ -43,7 +43,8 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[formattedDate.getDay()];
-  return `${day}, ${month} ${date}`;
+  let fullDate = `${day}, ${month} ${date}`;
+  return fullDate.toUpperCase();
 }
 
 function formatForecastDate(timestamp) {
@@ -97,6 +98,7 @@ function getDailyForecast(coordinates) {
   let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayDailyForecast);
+  console.log(apiUrl);
 }
 
 function displayTemperature(response) {
